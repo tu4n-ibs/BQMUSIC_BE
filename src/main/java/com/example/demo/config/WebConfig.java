7a05a12285fddd298e9c35e57ce7d10427b6909d
van tuan vuong <tuamphann5@gmail.com>
2026-03-10 17:24:40 +0700

    add cros

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -1,5 +1,5 @@
 package com.example.demo.config;
-
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -13,4 +13,14 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/public/**")
                 .addResourceLocations("file:/C:/image-for-porject/");
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://bqmusicnewfe-production.up.railway.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
 }
