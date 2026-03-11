08f566defa60a033a5bb6be3353ff8b299d2d4a7
van tuan vuong <tuamphann5@gmail.com>
2026-03-11 12:37:27 +0700

    feat: use composite ID for reply notifications

--- src/main/java/com/example/demo/service/content_service/CommentService.java
+++ src/main/java/com/example/demo/service/content_service/CommentService.java
@@ -60,16 +60,19 @@ public class CommentService {
                 .post(post)
                 .user(user)
                 .parent(parentComment)
-                .depth(depth)
                 .build();
+
+        CommentEntity savedComment = commentRepository.save(comment);
+
         notificationService.send(user, post.getUserEntity(),
                 ActionType.COMMENT, TargetNotiType.POST, post.getId());
 
         if (parentComment != null) {
+            String compositeId = post.getId() + ":" + savedComment.getId() + ":" + parentComment.getId();
             notificationService.send(user, parentComment.getUser(),
-                    ActionType.COMMENT, TargetNotiType.COMMENT, post.getId());
+                    ActionType.COMMENT, TargetNotiType.COMMENT, compositeId);
         }
-        return toResponse(commentRepository.save(comment));
+        return toResponse(savedComment);
     }
 
     public Page<CommentResponse> getRootComments(String postId, Pageable pageable) {
